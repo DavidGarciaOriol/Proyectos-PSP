d9686e1b71f36f85395341fdc29d645e839c0fb7
ciclot <ciclot@iesmartinezm.es>
2024-01-11 16:35:43 +0100

    Tarea Jefes Trabajadores Multihilo

---
+++ dgo-psp-u2-t1/src/main/java/iesmartinezm/psp/practica1/dgo/psp/u2/t1/DgoPspU2T1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package iesmartinezm.psp.practica1.dgo.psp.u2.t1;
+
+/**
+ *
+ * @author ciclot
+ */
+public class DgoPspU2T1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
